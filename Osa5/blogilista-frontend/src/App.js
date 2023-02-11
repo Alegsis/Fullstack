34741ec8331d8e87230148f3a1d49bea8917225e
Aleksi Virtanen <aleksvir@uef.fi>
2023-02-12 00:11:58 +0200

    aloitustilanne osa5
    .

---
+++ Osa5/blogilista-frontend/src/App.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )  
+  }, [])
+
+  if (user === null) {
+    return (
+        <div>
+          <h2>Log in to application</h2>
+          <form>
+            //...
+          </form>
+        </div>
+    )
+  }
+
+  return (
+    <div>
+      <h2>blogs</h2>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </div>
+  )
+}
+
+export default App
